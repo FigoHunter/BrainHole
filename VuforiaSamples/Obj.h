6542d814eb6c7ba749b022bd3d0f2f938edde1f5
赵亦飞 <figozhao96@gmail.com>
2016-07-20 15:59:28 +0800

    http connection added
    http connection added to AR

---
+++ VuforiaSamples/Obj.h
@@ -0,0 +1,17 @@
+//
+//  Object.h
+//  VuforiaSamples
+//
+//  Created on 16/6/26.
+//  Copyright © 2016年 Qualcomm. All rights reserved.
+//
+
+#ifndef Obj_h
+#define Obj_h
+static int indexNum;
+static float *vertices;
+static float *coords;
+static float *normals;
+static unsigned short *indices;
+
+#endif /* Obj_h */
